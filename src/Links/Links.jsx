dc0455d6dacb953772bcc43b5a198d9dfbe137f4
Antonio <jconradolopez19@gmail.com>
2024-07-28 09:05:07 -0600

    updated links

--- src/Links/Links.jsx
+++ src/Links/Links.jsx
@@ -2,7 +2,7 @@ const moviePopular = 'discover/movie?include_adult=false&include_video=false&lan
 const topRated = 'discover/movie?include_adult=false&include_video=false&language=es-US&page=1&sort_by=vote_average.desc&without_genres=99,10755&vote_count.gte=200';
 const seriesPopular = 'discover/tv?include_adult=false&language=es-US&page=1&sort_by=popularity.desc';
 const personPopular = 'person/popular?language=en-US&page=1';
-const topPeliculas = 'trending/all/day?language=en-US';
+const topPeliculas = 'trending/all/day?language=es-US';
 
 export {
     moviePopular,
